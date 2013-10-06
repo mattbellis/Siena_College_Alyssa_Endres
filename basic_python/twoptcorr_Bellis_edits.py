1117dde4044be422bcba5b8a0c56ce28983605cb
Matthew Bellis <matthew.bellis@gmail.com>
2013-10-06 11:06:16 -0400

    Almost have Bellis edits working.

--- basic_python/twoptcorr_Bellis_edits.py
+++ basic_python/twoptcorr_Bellis_edits.py
@@ -7,10 +7,11 @@ import matplotlib.pylab as plt
 ################################################################################
 # Distances calculation
 ################################################################################
-def distance(xpts0,ypts0,xpts1,ypts1,same='False'):
+def distance(x0,y0,x1,y1,same=False):
+    #print x0,y0,x1,y1,same
 
-    npts0 = len(xpts0)
-    npts1 = len(xpts1)
+    npts0 = len(x0)
+    npts1 = len(x1)
 
     N = 0
     if same is True:
@@ -20,12 +21,23 @@ def distance(xpts0,ypts0,xpts1,ypts1,same='False'):
 
     distances=np.zeros(N)    
 
+    i = 0
+    index_start = 0
+    index_end = npts1-1
+
+    if same is False:
+        index_end = npts1
+
     # Passed in the same arrays
+    print "same",same
+    print N
     if same is True:
+        print "here"
         i = 0
         index_start = 0
-        index_end = npts-1
-        for i in range(0,len(x)):
+        index_end = npts1-1
+        print "HERE"
+        for i in range(0,npts0):
 
             dist_new = np.sqrt((x0[i] - x1[i+1:])**2 + (y0[i] - y1[i+1:])**2)
             #print len(dist_new)
@@ -33,25 +45,34 @@ def distance(xpts0,ypts0,xpts1,ypts1,same='False'):
             distances[index_start:index_end] = dist_new[:]
             i += 1
 
-            index_start += (npts-i)
-            index_end = index_start + (npts-(i+1))
+            index_start += (npts1-i)
+            index_end = index_start + (npts1-(i+1))
 
         print "len: %d" % (len(distances))
 
     # Two arrays are different
     else:
-        for i in range(0,len(x)):
+        for i in range(0,npts0):
 
-            dist_new = np.sqrt((x0[i] - x1)**2 + (y[i] - y1)**2)
+            dist_new = np.sqrt((x0[i] - x1)**2 + (y0[i] - y1)**2)
             
             index = i*npts1
-            index = (i+1)*npts1
+            index_end = (i+1)*npts1
             distances[index:index_end] = dist_new
+
+            index_start += npts1
+            index_end = index_start + npts1
+
 
         print "len: %d" % (len(distances))
 
+    return distances
 
 
+
+################################################################################
+# Read in the data.
+################################################################################
 #infiled = open("C:\Users\Alyssa\Documents\GitHub\Siena_College_Alyssa_Endres\data\sample_pop1.dat")
 infiled = open("../data/sample_pop1.dat")
 #infiler = open("C:\Users\Alyssa\Documents\GitHub\Siena_College_Alyssa_Endres\data\sample_pop0.dat")
@@ -65,17 +86,6 @@ xd = vals[index]
 yd = vals[index+1]
 
 nptsd = len(xd)
-        
-    #plt.hist(distances,bins=100,range=(0,100))
-    #plt.show()
-distances=distance()
-plt.figure(figsize=(15,5))
-plt.subplot(1,3,1)
-dd = plt.hist(distances,bins=25,range=(0,1.5))
-N=(nptsd**2-nptsd)/2
-DD = dd[0] / float(N)
-
-###############################################################################
 
 
 vals = (np.array(infiler.read().split())).astype(float)
@@ -86,68 +96,30 @@ xr = vals[index]
 yr = vals[index+1]
 
 nptsr = len(xr)
+
         
-def distance():
-    #distances=[]
-    N=(nptsr**2-nptsr)/2
-    distances=np.zeros(N)    
-    i=0
-    j=0
-    count=0
-    for i in range (0,nptsr):
-        for j in range (i+1,nptsr):
-            x0=xr[i]
-            y0=yr[i]
-            x1=xr[j]
-            y1=yr[j]
-    
-            sq1 = (x1-x0)**2
-            sq2 = (y1-y0)**2
-            dist = math.sqrt(sq1 + sq2)
-            #distances.append(dist)
-            distances[count]=dist
-            count+=1            
-            j+=1
-        i+=1       
-    return distances        
-    #plt.hist(distances,bins=100,range=(0,100))
-    #plt.show()
-distances=distance()
+################################################################################
+# Do the calculations
+################################################################################
+plt.figure(figsize=(15,5))
+
+print "Starting DD........."
+distances=distance(xd,yd,xd,yd,same=True)
+plt.subplot(1,3,1)
+dd = plt.hist(distances,bins=25,range=(0,1.5))
+N=(nptsd**2-nptsd)/2
+DD = dd[0] / float(N)
+
+print "Starting RR........."
+distances=distance(xr,yr,xr,yr,same=True)
 plt.subplot(1,3,2)
 rr = plt.hist(distances,bins=25,range=(0,1.5))
 N=(nptsr**2-nptsr)/2
 RR = rr[0] / float(N)
 
 
-
-
-
-def distance():
-    #distances=[]
-    N=nptsr*nptsd
-    distances=np.zeros(N)    
-    i=0
-    j=0
-    count=0
-    for i in range (0,nptsr):
-        for j in range (0,nptsd):
-            x0=xr[i]
-            y0=yr[i]
-            x1=xd[j]
-            y1=yd[j]
-    
-            sq1 = (x1-x0)**2
-            sq2 = (y1-y0)**2
-            dist = math.sqrt(sq1 + sq2)
-            #distances.append(dist)
-            distances[count]=dist
-            count+=1            
-            j+=1
-        i+=1       
-    return distances        
-    #plt.hist(distances,bins=100,range=(0,100))
-    #plt.show()
-distances=distance()
+print "Starting DR........."
+distances=distance(xd,yd,xr,yr,same=False)
 plt.subplot(1,3,3)
 dr = plt.hist(distances,bins=25,range=(0,1.5))
 N=nptsr*nptsd
