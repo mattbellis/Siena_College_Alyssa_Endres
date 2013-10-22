43748aed18049c3f79927544b68d17248251e753
aa07endr <aa07endr@siena.edu>
2013-10-22 17:52:03 -0400

    Order Wechsler data and write to 91 files

--- basic_python/wechsler_data.py
+++ basic_python/wechsler_data.py
@@ -5,7 +5,7 @@ import math
 import numpy as np
 import matplotlib.pylab as plt
 
-infiler = open("C:\Users\Alyssa\Documents\GitHub\Siena_College_Alyssa_Endres\data\wechsler_gals_100k.dat")
+infiled = open("C:\Users\Alyssa\Documents\GitHub\Siena_College_Alyssa_Endres\data\wechsler_gals_100k.dat")
 
 vals = (np.array(infiled.read().split())).astype(float)
 nentries = len(vals)
@@ -15,4 +15,30 @@ r = vals[index]
 d = vals[index+1]
 z = vals[index+2]
 
-nptsd = len(r)
+npts = len(r)
+
+# indicies of sorted values
+sorted_indices = np.argsort(z)
+
+
+# lines per file
+nlines = 10000
+start_increment=1000
+nfiles=npts/(1.*start_increment)
+nfilemax=npts-nlines
+
+
+# ourput file- z, r, d
+for j in range (0,91):
+    filename = 'C:\Users\Alyssa\Documents\Python Scripts\wechsler'+'output_'+str(j)+'.dat'
+    output=open(filename,'w')
+    for i in range(nlines):    
+        index=start_increment*j+i    
+        if index > (npts-1):
+            break
+        output.write('%6.5e %6.5e %6.5e \n'%(z[sorted_indices[index]],r[sorted_indices[index]],d[sorted_indices[index]]))
+    output.close()
+        
+                
+
+
