c756ab76c9a9ce12315b52ea0c0905250c893bcb
aa07endr <aa07endr@siena.edu>
2013-11-12 12:53:23 -0500

    Trying to sync things.....

--- basic_python/wechsler_generalized.py
+++ basic_python/wechsler_generalized.py
@@ -15,42 +15,36 @@ r = vals[index]
 d = vals[index+1]
 z = vals[index+2]
 
-# sort galxies by z, return indices
-sorted_indices = np.argsort(z)
-r_sort = r[sorted_indices[index]]
-d_sort = d[sorted_indices[index]]
-z_sort = z[sorted_indices[index]]
-
-# plot histogram of z values
-plt.hist(z,bins=20,range=(0,5))
-plt.title("Sample Pop 0 y")
-plt.show()
-
-
-## IndexError: index 10895781 is out of bounds for size 10895780
+# plot histogram of all z values
+plt.hist (z, bins=50, range=(0, 0.35))
+plt.title ("range of z")
+plt.show ()
 
 
 #npts = len(r)
+## 10,895,780
 #
 ## lines per file
-#nlines = 10000
-#start_increment=1000
-#nfiles=npts/(1.*start_increment)
-#nfilemax=npts-nlines
-#
-#
-## ourput file- z, r, d
-#for j in range (0,91):
-#    #filename = 'C:\Users\Alyssa\Documents\Python Scripts\wechsler'+'output_'+str(j)+'.dat'
-#    filename = 'output_wechsler'+'output_'+str(j)+'.dat'
-#    output=open(filename,'w')
-#    for i in range(nlines):    
-#        index=start_increment*j+i    
-#        if index > (npts-1):
-#            break
-#        output.write('%6.5e %6.5e %6.5e \n'%(z[sorted_indices[index]],r[sorted_indices[index]],d[sorted_indices[index]]))
-#    output.close()
-#        
-#                
+#nlines = 100000
+#start_increment = 10000
+#nfiles = npts / (1.*start_increment)
+#nfilemax = npts - nlines
 
 
+spread = 0.1
+step = 0.05
+
+# plots histogram for some z range
+# 0-10,000; 1,000-11,000; 2,000-12,000
+for j in range (0, 0.35/step):
+        start = step * j
+        end = start + spread
+        plt.hist (z[start < z <end], bins=25, range=(0, 0.35))
+        plt.show ()
+        
+        
+       
+        
+
+        
+                
