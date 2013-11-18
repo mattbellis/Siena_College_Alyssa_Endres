d51c81f5fb0611ad4720bc8c26d4684be030e3ae
Matthew Bellis <matthew.bellis@gmail.com>
2013-11-17 22:11:41 -0500

    Normalize numbers by sum

--- basic_python/GPU_corr.py
+++ basic_python/GPU_corr.py
@@ -5,7 +5,8 @@ import matplotlib.pylab as plt
 from matplotlib import pyplot
 
 # data-data
-infiled = open("C:\Users\Alyssa\Documents\GitHub\Siena_College_Alyssa_Endres\data\log10binning_GPU_10k_data_data_arcmin.dat")
+#infiled = open("C:\Users\Alyssa\Documents\GitHub\Siena_College_Alyssa_Endres\data\log10binning_GPU_10k_data_data_arcmin.dat")
+infiled = open("../data/log10binning_GPU_10k_data_data_arcmin.dat")
 vals = (np.array(infiled.read().split())).astype(float)
 nentries = len(vals)
 ncols = 3
@@ -14,11 +15,14 @@ dd_th_hi = vals[index]
 dd_th_lo = vals[index+1]
 dd = vals[index+2]
 
+dd /= sum(dd)
+
 dd_th_avg = (dd_th_hi + dd_th_lo) / 2
 
 
 # data-flat
-infiled = open("C:\Users\Alyssa\Documents\GitHub\Siena_College_Alyssa_Endres\data\log10binning_GPU_10k_data_flat_arcmin.dat")
+#infiled = open("C:\Users\Alyssa\Documents\GitHub\Siena_College_Alyssa_Endres\data\log10binning_GPU_10k_data_flat_arcmin.dat")
+infiled = open("../data/log10binning_GPU_10k_data_flat_arcmin.dat")
 vals = (np.array(infiled.read().split())).astype(float)
 nentries = len(vals)
 ncols = 3
@@ -27,11 +31,14 @@ df_th_hi = vals[index]
 df_th_lo = vals[index+1]
 df = vals[index+2]
 
+df /= sum(df)
+
 df_th_avg = (df_th_hi + df_th_lo) / 2
 
 
 # flat-flat
-infiled = open("C:\Users\Alyssa\Documents\GitHub\Siena_College_Alyssa_Endres\data\log10binning_GPU_10k_flat_flat_arcmin.dat")
+#infiled = open("C:\Users\Alyssa\Documents\GitHub\Siena_College_Alyssa_Endres\data\log10binning_GPU_10k_flat_flat_arcmin.dat")
+infiled = open("../data/log10binning_GPU_10k_flat_flat_arcmin.dat")
 vals = (np.array(infiled.read().split())).astype(float)
 nentries = len(vals)
 ncols = 3
@@ -39,6 +46,8 @@ index = np.arange(0,nentries,3)
 ff_th_hi = vals[index]
 ff_th_lo = vals[index+1]
 ff = vals[index+2]
+
+ff /= sum(ff)
 
 ff_th_avg = (ff_th_hi + ff_th_lo) / 2
 
